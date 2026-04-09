fa41a385f98e212253aad8d23d255cd1b34d294a
aboubakr senouci <senouci.aboubakr@gmail.com>
2026-04-09 21:17:58 +0100

    fix: redirect user to trend-search page on first launch

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import { Button } from "@heroui/react";
-import React from "react";
+import { redirect } from "next/navigation";
 
-function Home() {
-  return (
-    <div className="flex justify-center items-center text-8xl text-accent-foreground">
-      page
-      <Button variant="primary">Test</Button>
-    </div>
-  );
+export default function Home() {
+  redirect("/trend-search");
 }
-
-export default Home;
